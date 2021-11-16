4232978eb6f9276399d11da2d41bf7aa6cf2c7b5
ThomasBarnekow <thomas@barnekow.info>
2021-11-16 14:32:44 +0100

    Use ifconfig eth0

--- run-docker-azure-cosmos-emulator-linux.sh
+++ run-docker-azure-cosmos-emulator-linux.sh
@@ -2,7 +2,7 @@
 
 # Determine the IP address of the local machine.
 # This step is required when Direct mode setting is configured using Cosmos DB SDKs.
-ipaddr="`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -n 1`"
+ipaddr="`ifconfig eth0 | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | head -n 1`"
 
 # Run the image, creating a container called "azure-cosmos-emulator-linux".
 docker run \
@@ -12,7 +12,7 @@ docker run \
   -p 10253:10253 \
   -p 10254:10254 \
   -m 3g \
-  --cpus=1.0 \
+  --cpus=2.0 \
   --name=azure-cosmos-emulator-linux \
   -e AZURE_COSMOS_EMULATOR_PARTITION_COUNT=3 \
   -e AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE=true \
